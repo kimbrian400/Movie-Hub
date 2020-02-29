3f6ab6a587ee609cdbbe1aa048bef541383dc1f5
Brian Kimathi <kimbrian400@gmail.com>
2020-02-29 16:54:42 +0300

    Modified

--- app/src/main/java/com/kimbrian/moviehub/details/MovieDetailsActivity.java
+++ app/src/main/java/com/kimbrian/moviehub/details/MovieDetailsActivity.java
@@ -1,4 +1,39 @@
 package com.kimbrian.moviehub.details;
 
-public class MovieDetailsActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import com.kimbrian.moviehub.Constants;
+import com.kimbrian.moviehub.Movie;
+import com.kimbrian.moviehub.R;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras != null && extras.containsKey(Constants.MOVIE)) {
+                Movie movie = extras.getParcelable(Constants.MOVIE);
+                if (movie != null) {
+                    MovieDetailsFragment movieDetailsFragment = MovieDetailsFragment.getInstance(movie);
+                    getSupportFragmentManager().beginTransaction().add(R.id.movie_details_container, movieDetailsFragment).commit();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
